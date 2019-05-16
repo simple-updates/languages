4e5cfbc969232557ac79439816c92c9cb2b38147
localhostdotdev <localhostdotdev@protonmail.com>
2019-05-16 18:01:37 +0200

    moving more stuff around

--- _src/_code/_parser.rb
+++ _src/_code/_parser.rb
@@ -1,6 +1,8 @@
+require 'execjs'
+
 module Code_
   class Parser_
-    PARSER_FILE_ = File.join(File.dirname(__FILE__), '../..//build/languages.js')
+    PARSER_FILE_ = File.join(File.dirname(__FILE__), '../../build/languages.js')
 
     def initialize
       @_compiled = ExecJS.compile(File.read(PARSER_FILE_))
