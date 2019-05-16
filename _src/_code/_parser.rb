fe36859c4ff40b60347887ab27a88e9a1c775ae2
localhostdotdev <localhostdotdev@protonmail.com>
2019-05-16 18:03:36 +0200

    moving more stuff around

--- _src/_code/_parser.rb
+++ _src/_code/_parser.rb
@@ -2,14 +2,14 @@ require 'execjs'
 
 module Code_
   class Parser_
-    PARSER_FILE_ = File.join(File.dirname(__FILE__), '../../build/languages.js')
+    PARSER_FILE_ = File.join(File.dirname(__FILE__), '../../_build/_languages.js')
 
     def initialize
       @_compiled = ExecJS.compile(File.read(PARSER_FILE_))
     end
 
     def _parse(_input)
-      @_compiled.call('Languages.parse', _input)
+      @_compiled.call('_Languages.parse', _input)
     end
   end
 end
